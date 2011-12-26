36da378d3d32687952192066a058e7c9e0c749d9
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-26 21:10:53 +0100

    remove pseudo testing code from the CLI db creation script.

--- grano/manage.py
+++ grano/manage.py
@@ -9,29 +9,6 @@ manager = Manager(app)
 def createdb():
     """ Create the SQLAlchemy database. """
     db.create_all()
-    p = Person()
-    p.title = "Foo"
-    p.current = True
-    db.session.add(p)
-    db.session.flush()
-    b = Person()
-    b.title = "Bar"
-    b.current = True
-    db.session.add(b)
-    db.session.flush()
-    r = Social()
-    r.source_id = p.id
-    r.target_id = b.id
-    r.current = True
-    print r.source
-    db.session.add(r)
-    db.session.commit()
-
-    for rel in Relation.query.all():
-        print rel.target
-        print rel.source
-
-
 
 if __name__ == '__main__':
     manager.run()
