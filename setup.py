59088d0fbec96e2bcb95f0c6d292e8023fdd1edc
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-26 20:20:50 +0100

    Add Flask-Script to have CLI environment support.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=[
         'sqlalchemy==0.7.4',
         'Flask==0.8', 
+        'Flask-Script==0.3.1',
         'flask-sqlalchemy==0.15',
         'sqlalchemy-migrate==0.7.2'
     ],
