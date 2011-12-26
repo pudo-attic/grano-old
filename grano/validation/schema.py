da4d7f6bd31819ca439954c799b550b67e48af83
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-26 23:17:59 +0100

    Stash away some reserved terms for schema attributes.

---
+++ grano/validation/schema.py
@@ -0,0 +1,9 @@
+
+# placeholder
+
+INVALID_ATTRIBUTE_NAMES = ['id', 'current', 'serial', 'title', 'slug',
+    'type', 'incoming', 'outgoing', 'target', 'source', 'target_id',
+    'source_id', 'network', 'network_id', 'created_at']
+
+
+
