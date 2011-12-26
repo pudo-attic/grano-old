fb3e30296beef88ac08f17037478dff0657ae391
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-26 22:54:51 +0100

    Comments in schema source class.

--- grano/model/schema.py
+++ grano/model/schema.py
@@ -11,6 +11,14 @@ class SchemaSet(object):
     pass
 
 class Schema(object):
+    """ A schema defines a specific subtype of either an entity or a relation.
+    This can mean any graph element, such as a person, company or other actor
+    type for entities - or a type of social, economic or political link for a 
+    relation (e.g. ownership, school attendance, ..).
+
+    A schema is defined through a model structure that contains necessary 
+    metadata to handle the schema both internally and via the user interface.
+    """
 
     def __init__(self, parent_cls, data):
         self.parent_cls = parent_cls
@@ -28,7 +36,13 @@ class Schema(object):
         return self._cls
 
     def _make_cls(self):
+        """ Generate a new type, mapped through SQLAlchemy. This will be a 
+        joined subtable to either an entity or a relation and retain a copy
+        of its composite primary key plus any attributes defined in the 
+        schema. """
         prefix = self.parent_cls.__tablename__
+
+        # inherit primary key:
         cls = {
             '__tablename__': prefix + '_' + self.name,
             'id': db.Column(db.String(36), db.ForeignKey(prefix + '.id'),
@@ -36,18 +50,25 @@ class Schema(object):
             'serial': db.Column(db.Integer, db.ForeignKey(prefix + '.serial'),
                 primary_key=True)
             }
+
+        # set up inheritance:
         cls['__mapper_args__'] = {
                 'polymorphic_identity': self.name,
                 'inherit_condition': db.and_(
                     cls['id']==self.parent_cls.id,
                     cls['serial']==self.parent_cls.serial)
                 }
+
+        # set up the specific attributes:
         for attribute in self.attributes:
             cls[attribute.name] = db.Column(attribute.column_type)
         return type(self.name, (self.parent_cls,), cls)
 
 
 class Attribute(object):
+    """ Attributes are specific properties of a schema for either an entity or 
+    a relation. They materialize as columns on the joined sub-table for the 
+    schema. """
 
     def __init__(self, name, data):
         self.name = name
@@ -57,5 +78,6 @@ class Attribute(object):
 
     @property
     def column_type(self):
+        # TODO: do we also need some typecasting mechanism?
         return ATTRIBUTE_TYPES_DB[self.type]
 
