7156342ac83d1d3648191b0bf96669946312617c
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-31 13:38:34 +0100

    Support listing of types in schema registry.

--- grano/model/schema_registry.py
+++ grano/model/schema_registry.py
@@ -11,7 +11,12 @@ class SchemaRegistry(object):
     def get(self, type_, name):
         return self._registry[type_][name]
 
+    def list(self, type_):
+        return self._registry[type_].keys()
 
-
+    @property
+    def types(self):
+        return dict([(t.__name__.lower(), t) \
+            for t in self._registry.keys()])
 
 
