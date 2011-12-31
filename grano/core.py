1de8447d16927e2e9b8629659aa1bc539809d4d8
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-31 14:41:29 +0100

    Shut up more SAWarnings.

--- grano/core.py
+++ grano/core.py
@@ -1,6 +1,8 @@
 # shut up useless SA warning:
 import warnings; 
 warnings.filterwarnings('ignore', 'Unicode type received non-unicode bind param value.')
+from sqlalchemy.exc import SAWarning
+warnings.filterwarnings('ignore', category=SAWarning)
 
 from flask import Flask
 from flaskext.sqlalchemy import SQLAlchemy
