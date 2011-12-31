8ef1ec31bc365bc9ad08015b3742cf2073dc08da
Friedrich Lindenberg <friedrich@pudo.org>
2011-12-31 14:59:47 +0100

    Column type for JSON wrapped content.

--- grano/model/util.py
+++ grano/model/util.py
@@ -2,6 +2,8 @@ import re
 from uuid import uuid4
 from time import time
 from unidecode import unidecode
+from json import dumps, loads
+from sqlalchemy.types import Text, MutableType, TypeDecorator
 
 SLUG_RE = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
@@ -21,5 +23,22 @@ def make_id():
 
 def make_serial():
     return int(time()*1000)
+
+
+class JSONType(MutableType, TypeDecorator):
+    impl = Text
+
+    def __init__(self):
+        super(JSONType, self).__init__()
+
+    def process_bind_param(self, value, dialect):
+        return dumps(value)
+
+    def process_result_value(self, value, dialiect):
+        return loads(value)
+
+    def copy_value(self, value):
+        return loads(dumps(value))
+
 
 
