5d50ce15de78aa28c855cb2856c0f24f1c6ffb48
Friedrich Lindenberg <friedrich@pudo.org>
2012-03-04 09:52:34 +0100

    Basic backbone app with network edit form.

--- grano/static/js/app.js
+++ grano/static/js/app.js
@@ -1,56 +1,92 @@
-var grano = grano || {};
+// # Grano Backbone Models
+this.Grano = this.Grano || {};
+this.Grano.Config = this.Grano.Config || {};
+this.Grano.Models = this.Grano.Models || {};
+this.Grano.Collections = this.Grano.Collections || {};
+this.Grano.Views = this.Grano.Views || {};
 
-grano.Network = Backbone.Model.extend({
-    idAttribute: "slug"
-});
- 
-grano.NetworkCollection = Backbone.Collection.extend({
-    model: grano.Network,
-    url: "/api/1/networks"
+(function($, my) {
+
+$.fn.serializeObject = function() {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+      if (o[this.name] !== undefined) {
+          if (!o[this.name].push) {
+              o[this.name] = [o[this.name]];
+          }
+          o[this.name].push(this.value || '');
+      } else {
+          o[this.name] = this.value || '';
+      }
+  });
+  return o;
+};
+
+my.Models.Network = Backbone.Model.extend({
+  idAttribute: 'slug',
+  url : function() {
+    if (this.isNew()) 
+      return my.Config.ApiEndpoint + 'networks';
+    return my.Config.ApiEndpoint + this.id;
+  }
 });
 
-grano.NetworkListView = Backbone.View.extend({
-    el: $('#network-list'),
-    initialize: function() {
-        this.model.bind("reset", this.render, this);
-    },
-    render: function(eventName) {
-        _.each(this.model.models, function(network) {
-            $(this.el).append(
-                new grano.NetworkListItemView({model: network}).render().el);
-        }, this);
-        return this;
-    }
-});
- 
-grano.NetworkListItemView = Backbone.View.extend({
-    tagName: "li",
-    template: _.template($('#network-list-item').html()),
-    render: function(eventName) {
-        $(this.el).html(this.template(this.model.toJSON()));
-        return this;
-    }
+
+my.Views.NetworkEdit = Backbone.View.extend({
+  events: {
+      "submit form": "save"
+  },
+    
+  initialize: function() {
+    this.template = Handlebars.compile($("#network-edit").html());
+    this.render();
+  },
+
+  save: function() {
+    var data = $(this.el).find("form").serializeObject();
+    console.log(data);
+    this.model.save(data, {
+      success: function(model, resp) {},
+      error: function(model, resp) {
+        console.log(resp);
+      }
+    });
+    return false;
+  },
+  
+  render: function() {
+    var html = this.template(this.model.toJSON());
+    $(this.el).html(html);
+    $('#app').html(this.el);
+  }  
+
 });
 
-var AppRouter = Backbone.Router.extend({
-    routes: {
-        ""               : "list",
-        "networks/:slug" : "networkDetails"
-    },
- 
-    list: function() {
-        console.log('list');
-        this.networkList = new grano.NetworkCollection();
-        this.networkListView = new grano.NetworkListView({model: this.networkList});
-        this.networkList.fetch();
-    },
- 
-    networkDetails: function(slug) {
-        this.network = this.networkList.get(slug);
-        this.networkView = new grano.NetworkView({model: this.wine});
-        this.networkView.render();
-    }
+my.App = Backbone.Router.extend({
+
+  routes: {
+    "networks":              "listNetworks",
+    "networks/new":          "newNetwork",
+    "networks/:slug":        "viewNetwork"
+  },
+
+  initialize: function(options) {
+    my.Config.ApiEndpoint = '/api/1/';
+  },
+
+  listNetworks: function() {
+    console.log("Banana!");
+  },
+
+  newNetwork: function() {
+    new my.Views.NetworkEdit({ model: new my.Models.Network() });
+  },
+
+  viewNetwork: function(slug) {
+    
+  }
+
 });
- 
-var app = new AppRouter();
-Backbone.history.start();
+
+}(jQuery, this.Grano));
