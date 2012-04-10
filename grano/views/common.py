67938d4a949d78d0688f154a077e71352178fc99
Friedrich Lindenberg <friedrich@pudo.org>
2012-04-10 09:41:05 +0200

    Rank by search score.

--- grano/views/common.py
+++ grano/views/common.py
@@ -14,16 +14,17 @@ def split_fields(field):
 def filtered_query(type_, request, fts=False):
     query = type_.all()
     try:
-        if fts:
-            q = request.args.get('q', '').strip()
-            if len(q):
-                query = query.filter('_fts @@ to_tsquery(:q)')
-                query = query.params(q=q)
         filter_ = [split_fields(f) for f in request.args.getlist('filter')]
         for key, values in groupby(filter_, lambda a: a[0]):
             attr = getattr(type_, key)
             clause = db.or_(*[attr == v[1] for v in values])
             query = query.filter(clause)
+        if fts:
+            q = request.args.get('q', '').strip()
+            if len(q):
+                query = query.filter('_fts @@ to_tsquery(:q)')
+                query = query.order_by('ts_rank_cd(_fts, plainto_tsquery(:q)) DESC')
+                query = query.params(q=q)
         count = query.count()
         query = query.limit(min(10000, int(request.args.get('limit', 100))))
         query = query.offset(int(request.args.get('offset', 0)))
